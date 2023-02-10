2f30c1d99b5a15e929ecae7e60e06b56e724134e
shubanms <msshuban4@gmail.com>
2023-02-11 00:45:44 +0530

    Added generate dot function

--- Project/gridGenerator.py
+++ Project/gridGenerator.py
@@ -6,7 +6,7 @@ class gridTester:
     def __init__(self , rows , columns , dots , grid):
         self.rows = rows
         self.columns = columns
-        self.dots = dots
+        self.dots = dots * 2
         self.grid = grid
         
     def grid_print(self , grid):
@@ -15,24 +15,32 @@ class gridTester:
         
     def place_dots(self , dots_positions):
         for items in dots_positions:
-            for item in items:
-                self.grid[item[0]][item[1]] = 'x'
+            self.grid[items[0]][items[1]] = 'x'
+                
+    def generate_dots_location(self):
+        dot_x = random.randint(3,10)
+        dot_y = random.randint(3,10)
+        
+        return [dot_x , dot_y]
         
     def generate_dots_positions(self):
         dots_positions = []
         for _ in range(self.dots):
             dot_placements = []
             
-            dot_placements.append(random.randint(3,10))
-            dot_placements.append(random.randint(3,10))
             
-            dots_positions.append(dot_placements)
             
         print(dots_positions)
         
         return dots_positions
             
-
+def check_valid_number_dots(dots , rows , columns):
+    if(dots > max(rows , columns)+1):
+        dots = dots % ((max(rows , columns)//2)+1)
+    else:
+        pass
+    
+    return dots
 
 def makeGrid(rows , columns):
     
@@ -44,7 +52,7 @@ if __name__ == "__main__":
     rows = random.randint(3,10)
     columns = random.randint(3,10)
     
-    number_of_dots = int(input("Enter the number of dots: "))
+    number_of_dots = check_valid_number_dots(int(input("Enter the number of dots: ")) , rows , columns)
     
     grid = makeGrid(rows , columns)
     
