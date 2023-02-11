c84bdb494518cda092be83a18e00dfe4a2736e29
shubanms <msshuban4@gmail.com>
2023-02-11 15:45:35 +0530

    Added generate functions

---
+++ Project/functions.py
@@ -0,0 +1,30 @@
+import numpy as np
+from pandas import *
+import random
+
+class gridFunc:
+    def __init__(self , rows : int , columns : int , dots : int , grid : list) -> None:
+        self.rows = rows
+        self.columns = columns
+        self.dots = dots
+        self.grid = grid
+        
+    def print_grid_spacing(self) -> None:
+        grid = np.array(self.grid)
+        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
+        
+    def print_grid_dataframe(self) -> None :
+        print(DataFrame(self.grid))
+        
+    def generate_random_blank_grid(self , place_holder : int) -> list:
+        rows = random.randint(3,10)
+        columns = random.randint(3,10)
+        
+        grid = [[place_holder]*rows]*columns
+        
+        return np.array(grid)
+    
+    def generate_blank_gird(self , rows : int , columns : int , place_holder : int) -> list:
+        grid = [[place_holder]*rows]*columns
+        
+        return np.array(grid)
